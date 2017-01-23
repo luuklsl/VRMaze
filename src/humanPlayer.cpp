2e66f9f57caaab74b232ac172887370e3cd541e7
Luuk Lenders <luuklsll@gmail.com>
2017-01-23 08:39:03 +0100

    Todo for human player

--- src/humanPlayer.cpp
+++ src/humanPlayer.cpp
@@ -4,7 +4,13 @@
 
 bool Grid::humanPlayer(GridElement* human_elem, int key_id)
 {
+	//left right up down: 356, 357, 358, 359
+	//also the only key id we should EVER get in here
 	bool moved = false;
 	//check if move is valid, if so, switch the bool
+	
+	//also move the character's flag (bool human in GridElem)
+	//move according to way player is facing
+
 	return moved;
 }
